8c53263dff081d4e75293eb79c395fcc176a7c32
floresvini050 <floresvinicius050@gmail.com>
2024-12-09 10:33:39 -0300

    Add files via upload
    Segundo desafio do CC50. Aqui, a ideia era criar duas pirâmides idênticas de acordo com as preferências do usuário.

---
+++ semana 1/mariodesafio.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int number;
+    do
+    {
+        printf("How big do you want the pyramid to be? ");
+        scanf("%d", &number);
+
+        if (number < 1)
+        {
+            printf("Invalid number. Only numbers bigger than 0!\n");
+        }
+
+    } while (number < 1);
+    
+    
+
+     for (int i = 1; i <= number; i++)
+    {
+        for (int a = 0; a < i; a++)
+        {
+            printf("#");
+        }
+
+        printf(" ");
+
+        for (int b = 0; b < i; b++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
